7471b98645c92c0ec7d35646365f978bef0ef07d
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-10-06 12:46:09 +0530

    updated commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,11 +1,12 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
-const fs = require('node:fs');
-const path = require('node:path');
-
+const { REST, Routes } = require("discord.js");
+const fs = require("node:fs");
+require("dotenv").config();
+const path = require("node:path");
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENTID;
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
@@ -14,15 +15,19 @@ for (const folder of commandFolders) {
     if (fs.statSync(currentPath).isDirectory()) {
         // Grab all the command files from the commands directory you created earlier
         const commandsPath = path.join(foldersPath, folder);
-        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+        const commandFiles = fs
+            .readdirSync(commandsPath)
+            .filter((file) => file.endsWith(".js"));
         // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
         for (const file of commandFiles) {
             const filePath = path.join(commandsPath, file);
             const command = require(filePath);
-            if ('data' in command && 'execute' in command) {
+            if ("data" in command && "execute" in command) {
                 commands.push(command.data.toJSON());
             } else {
-                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+                console.log(
+                    `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+                );
             }
         }
     }
@@ -34,15 +39,18 @@ const rest = new REST().setToken(token);
 // and deploy your commands!
 (async () => {
     try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-        // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(
-            Routes.applicationCommands(clientId),
-            { body: commands },
+        console.log(
+            `Started refreshing ${commands.length} application (/) commands.`
         );
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(Routes.applicationCommands(clientId), {
+            body: commands,
+        });
+
+        console.log(
+            `Successfully reloaded ${data.length} application (/) commands.`
+        );
     } catch (error) {
         // And of course, make sure you catch and log any errors!
         console.error(error);
